b08799e898979379f6b2ee5169b9e03be4756cef
agito <1634263395@qq.com>
2024-01-15 20:20:52 +0800

    some optimizations

--- Token.sol
+++ Token.sol
@@ -2,23 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-/*
-发行一个具备回调功能的 ERC20Token , 加入回调token
-
-tokensReceived
-
-• 保持原有逻辑下,扩展 TokenBank, 实现在转账回调中,将
-
-Token 存入 TokenBank
-
-• 转账用 OpenZeppelin 的 safeTransferFrom
-*/
-
-
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 interface IBank {
-        function tokenRecieve(address user, uint amount) external;
+        function tokensRecieved(address user, uint amount) external;
 } 
 
 contract btcToken is ERC20{
@@ -29,6 +16,9 @@ contract btcToken is ERC20{
 
     address private owner;
     error NotOwner(address user);
+    event MintSuccess(address user, uint amount);
+    event DepositSuccess(address user, address bank, uint amount);
+
     modifier onlyOwner{
         if(msg.sender != owner) revert NotOwner(msg.sender);
         _;
@@ -37,13 +27,14 @@ contract btcToken is ERC20{
    //铸造代币给对应用户
    function distributeTokens(address receiver, uint amount) public onlyOwner{
         _mint(receiver, amount);
+        emit MintSuccess(receiver, amount);
    }
    
    //用户直接向目标地址存款
    function deposit(address bank, uint amount) public{
-         _transfer(msg.sender, bank, amount);
-        IBank(bank).tokenRecieve(msg.sender, amount);
-
+        _transfer(msg.sender, bank, amount);
+        IBank(bank).tokensRecieved(msg.sender, amount);
+        emit DepositSuccess(msg.sender, bank, amount);
    }
 
 
