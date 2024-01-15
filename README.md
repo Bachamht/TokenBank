bdc8041fb542e1e6ed47a177f291d1a77551c825
agito <1634263395@qq.com>
2024-01-15 20:24:20 +0800

    add images

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 用户调用代币合约直接向目标银行合约存款（不需要approve）：
 
-![image]()
+![image](https://github.com/Bachamht/TokenBank/blob/main/images/cc35463f4a7e48820c0effcaccdca103.png)
 
 
 
 用户先approve，给银行合约代币使用权限：
+![image](https://github.com/Bachamht/TokenBank/blob/main/images/364813a0ab11ab2b671f84f0a528734b.png)
 
 
 
 用户再调用银行合约进行存款：
+![image](https://github.com/Bachamht/TokenBank/blob/main/images/620204d45ccb0041ba102f3b4d9200a0.png)
--- TokenBank.sol
+++ TokenBank.sol
@@ -1,47 +1,50 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
-/*
-发行一个具备回调功能的 ERC20Token , 加入回调token
-
-tokensReceived
-
-• 保持原有逻辑下,扩展 TokenBank, 实现在转账回调中,将
-
-Token 存入 TokenBank
-
-• 转账用 OpenZeppelin 的 safeTransferFrom
-*/
-
-interface IbtcToken {
-        function  transferFrom(address from, address to, uint256 value) external;
-        function  approve(address spender, uint256 value) external;
-} 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TokenBank  {
 
+    using SafeERC20 for IERC20;
+
     address private administrator;
+    address private tokenPool;
     mapping(address => uint) private balances;
+
     error NotAdministrator(address user);
-    modifier onlyAdministrator{
+    error NotCorrectToken(address tokenPool);
+
+    event TransferSuccess(address from, address bank, uint amount);
+    event WithdrawSuccess(address receiver, uint amount);
+
+    modifier onlyAdministrator {
         if(msg.sender != administrator) revert NotAdministrator(msg.sender);
         _;
     }
+   
+    //限定只能由规定的代币池向这个合约打钱
+    modifier onlyTokenPool {
+        if (msg.sender != tokenPool) revert NotCorrectToken(msg.sender);
+        _;
+    }
 
-    constructor() {
+    constructor(address TokenPool) {
         administrator = msg.sender;
+        tokenPool = TokenPool;
     }
 
     //用户存款
     function deposit(address token, uint amount) public {
-        IbtcToken(token).transferFrom(msg.sender, address(this), amount);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
         balances[msg.sender] += amount;
+        emit TransferSuccess(msg.sender, address(this), amount);
     }
     
     //管理员撤资
     function withdraw(address token) public  onlyAdministrator{
-        IbtcToken(token).approve(administrator, address(this).balance);
-        IbtcToken(token).transferFrom(address(this), administrator, address(this).balance);
+        IERC20(token).approve(administrator, address(this).balance);
+        IERC20(token).safeTransferFrom(address(this), administrator, address(this).balance);
+        emit WithdrawSuccess(administrator, address(this).balance);
     }
     
     //用户查看存款
@@ -50,8 +53,10 @@ contract TokenBank  {
     }
 
     //对用户直接转进地址的代币进行处理
-    function tokenRecieve(address user, uint amount) external {
+    function tokensRecieved(address user, uint amount) external onlyTokenPool{
         balances[user] += amount;
+        emit TransferSuccess(user, address(this), amount);
+
     }
 
 }
